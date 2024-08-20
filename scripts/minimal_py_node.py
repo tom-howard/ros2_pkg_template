b6da8ca5ea2d719131482f70118378e3af9f43fd
Tom Howard <t.howard@sheffiedl.ac.uk>
2024-08-20 16:50:24 +0100

    minimal python node content

--- scripts/minimal_py_node.py
+++ scripts/minimal_py_node.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+# minimal Python node courtesy of Robotics Backend:
+# https://roboticsbackend.com/write-minimal-ros2-python-node/
+
+
+import rclpy
+from rclpy.node import Node
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = Node('my_node_name')
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
