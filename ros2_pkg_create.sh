83660aa32c4b6908674cce76b36a04858a517438
Tom Howard <t.howard@sheffiedl.ac.uk>
2024-08-21 21:41:00 +0100

    catch no ros2 workspaces or if the pkg already exists

--- ros2_pkg_create.sh
+++ ros2_pkg_create.sh
@@ -21,8 +21,12 @@ ask() {
 # cd $PKG_NAME 
 # rm -rf .git
 
+if [ -z "$COLCON_PREFIX_PATH" ]; then
+    echo "[EXITING] No ROS2 Workspaces detected."
+    exit 0
+fi
+
 ROS2_WS=""
-# Replace colons with spaces to create list.
 for path in ${COLCON_PREFIX_PATH//:/ }; do
     targetdir="$(dirname "$path")/src"
     echo "ROS2 Workspace detected at: '$targetdir'."
@@ -35,7 +39,7 @@ for path in ${COLCON_PREFIX_PATH//:/ }; do
 done
 
 if [ -z "$ROS2_WS" ]; then
-    echo "No path selected, exiting."
+    echo "[EXITING] No path selected."
     exit 0    
 fi
 
@@ -43,10 +47,24 @@ echo -n "[INPUT] Please enter a name for your package >>> "
 read -r PKG_NAME </dev/tty
 
 if [ -z "$PKG_NAME" ]; then
-    echo "No package name provided, exiting."
+    echo "[EXITING] No package name provided."
     exit 0
 fi
 
 PKG_PATH="$ROS2_WS/$PKG_NAME"
+
+if [ -d "$PKG_PATH"]; then
+    echo "[WARNING] The '$PKG_NAME' ROS package (or a directory of the same name) already exists!"
+    if ask "Do you want to replace it?"; then
+        echo "Removing the existing directory:"
+        echo "  $PKG_PATH"
+        rm -rf $PKG_PATH
+    else
+        echo "[EXITING] Start again with a new package name, or remove the existing one."
+        exit 0
+    fi
+fi
+
 echo "Creating the '$PKG_NAME' package at:"
 echo "  $PKG_PATH"
+
