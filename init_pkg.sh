232bc232afa66ace9edb94968c7aaf6dcbf3bc01
Tom Howard <t.howard@sheffield.ac.uk>
2025-07-03 09:11:58 +0100

    Add dbg func

--- init_pkg.sh
+++ init_pkg.sh
@@ -14,13 +14,20 @@ ask() {
     fi
 }
 
+dbg() {
+    DEBUG=1 # Set DEBUG= to dissable debug messages
+    if [[ -n "${DEBUG}" ]]; then
+        echo "[DBG] $1"
+    fi
+}
+
 SCRIPT_NAME="$(readlink -f "$0")"
 PKG_PATH="$(dirname "${SCRIPT_NAME}")"
 
-echo "[DBG] Package path: ${PKG_PATH}"
+dbg "Package path: ${PKG_PATH}"
 
 cd ${PKG_PATH}
-echo "[DBG] Current (PKG) directory: $(pwd)"
+dbg "Current (PKG) directory: $(pwd)"
 
 if ! $(git status >/dev/null 2>&1); then
     echo "[ERROR] Not a git repository..."
@@ -29,31 +36,31 @@ fi
 
 GIT_REMOTE_URL=$(git config --get remote.origin.url)
 if [[ "${GIT_REMOTE_URL}" == "https://github.com/tom-howard/ros2_pkg_template.git" ]]; then
-    echo "[DBG] this is the package template"
+    dbg "this is a clone of the package template"
     PKG_NAME=$1
     FROM_TEMPLATE=True
 else
-    echo "[DBG] this is not the package template, its a fork."
+    dbg "this is not the package template, preserve git."
     PKG_NAME=$(basename "${GIT_REMOTE_URL}" ".git")
     FROM_TEMPLATE=False
 fi
 
-echo "[DBG] Package name: ${PKG_NAME}"
+dbg "Package name: ${PKG_NAME}"
 
 if [[ -z "${PKG_NAME}" ]]; then
-    echo "[DBG] Package name is empty."
+    dbg "Package name is empty."
     echo -n "[INPUT] Please enter a name for your package >> "
     read -r ${PKG_NAME} </dev/tty
 fi
 
-echo "[DBG] Package name: ${PKG_NAME}"
+dbg "Package name: ${PKG_NAME}"
 
 if [[ ! "${PKG_NAME}" =~ ^[a-z][a-z0-9_]*$ ]]; then
     echo "[ERROR] Invalid package name: '${PKG_NAME}'."
     echo "Package names must start with a lowercase letter, and can only contain lowercase letters, numbers, and underscores."
     exit 255
 else
-    echo "[DBG] Package name OK"
+    dbg "Package name OK"
 fi
 
 if [ -z "$COLCON_PREFIX_PATH" ]; then
@@ -67,19 +74,19 @@ PKG_PATH_IN_WS=false
 for ws in "${COLCON_WS[@]}"; do
     if [[ "${PKG_PATH}" == "$(dirname "${ws}")/src"* ]]; then
         PKG_PATH_IN_WS=true
-        echo "[DBG] PKG_PATH is inside workspace: $(dirname "${ws}")/src"
+        dbg "PKG_PATH is inside workspace: $(dirname "${ws}")/src"
         break
     fi
 done
 
 if [[ "${PKG_PATH_IN_WS}" == "true" ]]; then
-    echo "[DBG] PKG_PATH is inside a ROS2 workspace."
+    dbg "PKG_PATH is inside a ROS2 workspace."
 else
     if ! ask "This package doesn't appear to be inside a ROS2 workspace. Are you sure you want to continue?"; then
-        echo "[DBG] Not in a WS, don't continue."
+        dbg "Not in a WS, don't continue."
         exit 255
     fi
-    echo "[DBG] PKG_PATH is NOT inside any ROS2 workspace, proceeding anyway."
+    dbg "PKG_PATH is NOT inside any ROS2 workspace, proceeding anyway."
 fi
 
 if ! ask "Initialise this ROS package with the name '${PKG_NAME}'?"; then
@@ -90,9 +97,9 @@ fi
 echo "Initialising the '${PKG_NAME}' package..."
 
 if [[ "${FROM_TEMPLATE}" == "True" ]]; then
-    echo "[DBG] Renaming package location to '${PKG_NAME}'"
+    dbg "Renaming package location to '${PKG_NAME}'"
     NEW_PKG_PATH="$(dirname "${PKG_PATH}")/${PKG_NAME}"
-    echo "[DBG] New package location: ${NEW_PKG_PATH}"
+    dbg "New package location: ${NEW_PKG_PATH}"
     if [ -d "${NEW_PKG_PATH}" ]; then
         echo "[ERROR] The '${PKG_NAME}' ROS package (or a directory of the same name) already exists!"
         exit 255
